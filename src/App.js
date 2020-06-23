0e1012879990e461d07f664c2466c0ad4c6afe45
Sean Ross <seanross917@gmail.com>
2020-06-23 11:54:25 -0400

    created foodsTable component, search, & display in table

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FoodsTable from './components/FoodsTable';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    error: null,
+    isLoaded: true,
+    foods: [],
+  };
+
+  handleSearch = (e) => {
+    e.preventDefault();
+    const search = e.target.elements.search.value.trim();
+
+    if (search) {
+      const api =
+        "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DEMO_KEY";
+      const endpoint = encodeURI(api.concat(`&query=${search}`));
+
+      this.setState({ isLoaded: false });
+
+      fetch(endpoint)
+        .then((res) => res.json())
+        .then(res => {
+          // Refine search to look for exact match only
+          if (res.totalHits && res.totalHits !== 0) {
+            const foods = res.foods.filter(
+              (food) => food.description.toLowerCase() === search
+            );
+            return { foods };
+          } else {
+            return res;
+          }
+        })
+        .then(
+          (result) => {
+            console.log(result);
+
+            if (result.totalHits === 0) {
+              const error = { message: `No results found for: "${search}"` };
+
+              this.setState({
+                isLoaded: true,
+                error,
+                foods: [],
+              });
+            } else {
+              this.setState({
+                isLoaded: true,
+                error: null,
+                foods: result.foods,
+              });
+            }
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error,
+            });
+          }
+        );
+    }
+  };
+
+  render() {
+    const { error, isLoaded, foods } = this.state;
+
+    return (
+      <div className="App">
+        <h1>nutrition app</h1>
+        <form onSubmit={this.handleSearch}>
+          <input autoFocus type="text" name="search" />
+          <button>Search</button>
+          {error && <div>Error fetching data: {error.message}</div>}
+          {!isLoaded ? <div>Loading...</div> : <FoodsTable data={foods} />}
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/FoodsTable.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+// const foodNutrients = {
+//   1003: "Protein",
+//   1004: "Total lipid (fat)",
+//   1005: "Carbohydrate, by difference",
+//   1008: "Energy",
+//   1258: "Fatty acids, total saturated",
+// };
+
+const styles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+const getNutrientValue = (nutrients = [], id) => {
+  const nutrient = nutrients.filter(
+    (n) => n.nutrientId === id
+  );
+  return nutrient[0] ? nutrient[0].value : "-";
+};
+
+const FoodsTable = ({ data }) => {
+    return (
+      <TableContainer component={Paper}>
+        <Table className={styles.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Food (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data &&
+              data.map((food) => (
+                <TableRow key={food.fdcId}>
+                  <TableCell component="th" scope="row">
+                    {food.description}
+                  </TableCell>
+                  <TableCell align="right">
+                    {/** CALORIES */}
+                    {getNutrientValue(food.foodNutrients, 1008)}
+                  </TableCell>
+                  <TableCell align="right">
+                    {/** FAT */}
+                    {getNutrientValue(food.foodNutrients, 1004)}
+                  </TableCell>
+                  <TableCell align="right">
+                    {/** CARBS */}
+                    {getNutrientValue(food.foodNutrients, 1005)}
+                  </TableCell>
+                  <TableCell align="right">
+                    {/** PROTEIN */}
+                    {getNutrientValue(food.foodNutrients, 1003)}
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+}
+
+export default FoodsTable;
